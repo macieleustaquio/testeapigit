0decc330d9ea553667a0c32be5a719c2c1eb9f02
Maciel Eustaquio <maciel.seo@gmail.com>
2022-11-23 10:26:21 -0300

    Cria modal nos botoes da tabela.

--- testeapigit/src/app/components/content/content.component.ts
+++ testeapigit/src/app/components/content/content.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Octokit } from '@octokit/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent implements OnInit {
 
-  constructor() { }
+export class ContentComponent implements OnInit {
+  
+  constructor() {}
 
   ngOnInit(): void {
+    const octokit = new Octokit({
+      auth: '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'
+    })
+    
+    const res = async () => {
+      await octokit.request('GET /repos/macieleustaquio/pizza/issues?milestone,state,assignee,creator,mentioned,labels,sort,direction,since,per_page,page', {
+        owner: 'OWNER',
+        repo: 'REPO'
+      })
+    }
   }
 
 }
